ceea6b16a2f4df824bb3a3ea000d3a163c630c92
Matt Miller <thisismattmiller@gmail.com>
2022-12-14 18:08:30 -0500

    Update doc_loc_api.py

--- doc_loc_api.py
+++ doc_loc_api.py
@@ -8,19 +8,26 @@ payload = {
 
 r = requests.get(base_url, params=payload)
 data = json.loads(r.text)
+with open('feminism_results.csv', 'w') as file:
+ 
+ writer = csv.writer(file)
+ 
+ result_header = ["date", "shelf_id", "title", "original_format", "item_summary", "access_res"]
+ writer.writerow(result_header)
+ 
+ for item in data["content"]["results"]:
 
-for item in data["content"]["results"]:
+     # meow = item["item"]
+     purr = []
+     purr.append(item["date"])
+     purr.append(item["shelf_id"])
+     purr.append(item["title"])
+     purr.append(item["original_format"])
+     purr.append(item["item"]["summary"])
+     purr.append(item["access_restricted"])
 
-    meow = item["item"]
-    purr = []
-    purr.append(item["date"])
-    purr.append(item["shelf_id"])
-    purr.append(item["title"])
-    purr.append(item["original_format"])
-    purr.append(item["item"]["summary"])
-    purr.append(item["access_restricted"])
+     for resource in item["resources"]:
+         purr.append(resource)
 
-    for resource in item["resources"]:
-        purr.append(resource)
-
-    print(purr)
+     print(purr)
+     writer.writerow(purr)
