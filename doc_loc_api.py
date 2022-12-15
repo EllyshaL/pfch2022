eef225c358ffcc7658b6da35d99428657fbd98e3
EllyshaL <116770012+EllyshaL@users.noreply.github.com>
2022-12-15 16:08:44 -0500

    Add files via upload
    Library of Congress API Request for Film/Videos to fit documentary theme/keyword (interchangeable). "Feminist" and "Feminism" were used in this case.

---
+++ doc_loc_api.py
@@ -0,0 +1,37 @@
+import requests
+import json
+import csv
+
+base_url = "https://www.loc.gov/film-and-videos/?q=feminist&fo=json"
+payload = {
+ 'q' : 'feminist', 'feminism'
+ 'rows' : 1000
+}
+
+r = requests.get(base_url, params=payload)
+data = json.loads(r.text)
+with open('feminism_results.csv', 'w') as file:
+
+ writer = csv.writer(file)
+
+ result_header = ["date", "shelf_id", "title", "original_format", "item_summary", "access_res"]
+ writer.writerow(result_header)
+
+ for item in data["content"]["results"]:
+
+     # meow = item["item"]
+     purr = []
+     purr.append(item["date"])
+     purr.append(item["shelf_id"])
+     purr.append(item["title"])
+     purr.append(item["original_format"])
+     purr.append(item["item"]["summary"])
+     purr.append(item["access_restricted"])
+
+     if len(item["resources"]) > 0:
+         resource = item["resources"][0]
+         purr.append(resource["url"])
+         purr.append(resource["image"])
+
+     print(purr)
+     writer.writerow(purr)
